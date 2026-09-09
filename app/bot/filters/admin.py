0f4a78383c0f5eed6aebb8e5fa57d7caa91bd909
Kostya Skrebkov <kostaskrebkov1@gmail.com>
2026-09-09 10:53:39 +0300

    change admin.py

--- app/bot/filters/admin.py
+++ app/bot/filters/admin.py
@@ -1,12 +1,9 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message, CallbackQuery
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config import settings
-from app.database.repositories.user_repo import UserRepository
 
 
 class IsAdmin(BaseFilter):
-    async def __call__(self, event: Message | CallbackQuery, session: AsyncSession) -> bool:
-        user_repo = UserRepository(session)
-        return await user_repo.is_admin(event.from_user.id, settings.admin_ids_list)
+    async def __call__(self, event: Message | CallbackQuery) -> bool:
+        return event.from_user.id in settings.admin_ids_list
